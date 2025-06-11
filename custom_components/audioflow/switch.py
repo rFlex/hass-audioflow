3827005e6d84f1b036c1a4575d354c472acacd32
Simon Corsin <simon@corsin.me>
2025-06-11 10:13:19 -0500

    should reference self here

--- custom_components/audioflow/switch.py
+++ custom_components/audioflow/switch.py
@@ -69,11 +69,11 @@ class ZoneStateSwitch(ZoneEntity, SwitchEntity):
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         await self.coordinator.client.set_zone_state(self.zone_id, True)
-        await coordinator.async_refresh()
+        await self.coordinator.async_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         await self.coordinator.client.set_zone_state(self.zone_id, False)
-        await coordinator.async_refresh()
+        await self.coordinator.async_refresh()
 
     @property
     def name(self) -> str:
