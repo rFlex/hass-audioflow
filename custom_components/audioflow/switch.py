b3021f948b0ccbc190423382a5164a22fd389237
Simon Corsin <simon@corsin.me>
2025-06-11 08:55:01 -0500

    try with async_refresh

--- custom_components/audioflow/switch.py
+++ custom_components/audioflow/switch.py
@@ -69,13 +69,11 @@ class ZoneStateSwitch(ZoneEntity, SwitchEntity):
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         await self.coordinator.client.set_zone_state(self.zone_id, True)
-        self.af_zone["state"] = "on"
-        await self.coordinator.async_request_refresh()
+        await coordinator.async_refresh()
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         await self.coordinator.client.set_zone_state(self.zone_id, False)
-        self.af_zone["state"] = "off"
-        await self.coordinator.async_request_refresh()
+        await coordinator.async_refresh()
 
     @property
     def name(self) -> str:
